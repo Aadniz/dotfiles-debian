700c0ad6623fc68edeb2d255455f77cad9abcddc
Aadniz <8147434+Aadniz@users.noreply.github.com>
2025-08-07 00:59:52 +0200

    Various upgrades to niri config

--- niri/background.sh
+++ niri/background.sh
@@ -3,10 +3,8 @@
 swww-daemon -n "wp-front" &
 swww-daemon -n "wp-back"  &
 
-swww img "$@" -n "wp-front"
-
 # Blur the image
 convert -blur 0x25 "$@" /tmp/blurred-wallpaper.jpg
 
 swww img /tmp/blurred-wallpaper.jpg -n "wp-back"
-
+swww img "$@" -n "wp-front"
