6ef349c1120aa4aa79ccaa606872693cbbd247c9
Aadniz <8147434+Aadniz@users.noreply.github.com>
2026-02-23 04:15:23 +0100

    Updated fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set fish_greeting
 if status is-interactive
-    kotofetch --border false --centered false --horizontal-padding 1 --vertical-padding 1
+#    kotofetch --border false --centered false --horizontal-padding 1 --vertical-padding 1
 end
 
-source "$HOME/.cargo/env.fish"
+#source "$HOME/.cargo/env.fish"
 zoxide init --cmd cd fish | source
