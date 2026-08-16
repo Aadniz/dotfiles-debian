a52bed02f686c221c6cb373dfbdf8b39c2261e31
Aadniz <8147434+Aadniz@users.noreply.github.com>
2026-08-16 23:10:52 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,15 @@
 set fish_greeting
 if status is-interactive
-    kotofetch --border false --centered false --horizontal-padding 1 --vertical-padding 1
+    kotofetch --border false --centered false --horizontal-padding 1 --vertical-padding 1 --modes $HOME/.config/kotofetch/quotes/*
 end
+
+set --export ANDROID_HOME $HOME/Android/Sdk
+set -gx PATH $ANDROID_HOME/emulator $PATH;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+
+set --export JAVA_HOME /usr/lib/jvm/java-21-openjdk-amd64
 
 source "$HOME/.cargo/env.fish"
 zoxide init --cmd cd fish | source
